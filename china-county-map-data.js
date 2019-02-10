bfd981da1db1d58919dab7054e2be1ecdc77c72f
deqoder <leekon.shen@gmail.com>
2019-02-10 20:54:47 +0800

    add single color map

---
+++ china-county-map-data.js
@@ -0,0 +1,4 @@
+var jsondata = [
+{'county': '东兴', 'color': 'red', 'links': [ {'name': '从东兴出发', 'link': 'http://tv.cctv.com/2012/12/16/VIDE1355596345042594.shtml'}]},
+{'county': '防城港', 'color': 'red', 'links': [{'name': '山城防城港', 'link': 'http://tv.cctv.com/2013/01/04/VIDE1357298716231605.shtml'}]}
+];
