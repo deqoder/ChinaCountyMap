e7389fa91b73606502f22309b4fe28e28684d729
slk <leekon.shen@gmail.com>
2019-02-12 13:13:20 +0800

    add json config color and link

--- china-county-map-data.js
+++ china-county-map-data.js
@@ -1,4 +1,6 @@
 var jsondata = [
-{'county': '东兴', 'color': 'red', 'links': [ {'name': '从东兴出发', 'link': 'http://tv.cctv.com/2012/12/16/VIDE1355596345042594.shtml'}]},
-{'county': '防城港', 'color': 'red', 'links': [{'name': '山城防城港', 'link': 'http://tv.cctv.com/2013/01/04/VIDE1357298716231605.shtml'}]}
+{'county': '东兴', 'color': 'green', 'links': [ {'name': '从东兴出发', 'link': 'http://tv.cctv.com/2012/12/16/VIDE1355596345042594.shtml'}]},
+{'county': '防城港', 'color': 'blue', 'links': [{'name': '山城防城港', 'link': 'http://tv.cctv.com/2013/01/04/VIDE1357298716231605.shtml'}]},
+{'county': '宁明', 'color': 'green', 'links': [{'name': '山水看宁明', 'link': 'http://tv.cctv.com/2013/01/04/VIDE1357298716313685.shtml'}]},
+{'county': '凭祥', 'color': 'green', 'links': [{'name': '凭祥走边关', 'link': 'http://tv.cctv.com/2013/01/04/VIDE1357298716381743.shtml'}]}
 ];
